9c29a0d381287a82f11b6e09f5fa40930fba0fd5
jony <wjl1990720@sina.com>
2022-11-20 12:50:33 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,1 @@
-(function hitokoto(){var hitokoto="明月出天山，苍茫云海间。";var dom=document.querySelector('#hitokoto');Array.isArray(dom)?dom[0].innerText=hitokoto:dom.innerText=hitokoto;})()
+(function hitokoto(){var hitokoto="胜负之决，只在此心动与不动。";var dom=document.querySelector('#hitokoto');Array.isArray(dom)?dom[0].innerText=hitokoto:dom.innerText=hitokoto;})()
